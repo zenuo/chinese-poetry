5148ca35a155243918450088300d3cd12fd508dd
zenuo <zenuo@protonmail.com>
2019-05-16 11:38:40 +0800

    实现数据记录插入

--- mysql/README.md
+++ mysql/README.md
@@ -9,8 +9,9 @@ CREATE DATABASE `chinese-poetry` CHARACTER SET 'utf8mb4';
 ## 宋诗表
 
 ```sql
-CREATE TABLE `poet_song` (
+CREATE TABLE `poet` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `dynasty`varchar(11) DEFAULT NULL,
   `author` varchar(31) DEFAULT NULL,
   `paragraph` varchar(1023) DEFAULT NULL,
   `strains` varchar(1023) DEFAULT NULL,
--- mysql/main.go
+++ mysql/main.go
@@ -2,19 +2,49 @@ package main
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 // Poet 诗
 type Poet struct {
-	gorm.Model
 	ID        uint64 `gorm:"Column:id;auto_increment"`
 	Author    string `gorm:"Column:author"`
 	Paragraph string `gorm:"Column:paragraph"`
 	Strains   string `gorm:"Column:strains"`
 	Title     string `gorm:"Column:title"`
+	Dynasty   string `gorm:"Column:dynasty"`
+}
+
+func (Poet) TableName() string {
+	return "poet"
 }
 
 func main() {
+	// 打开数据库连接
+	db, _ := gorm.Open("mysql", "app:123456@tcp(localhost:3306)/chinese-poetry?charset=utf8&parseTime=True")
+	err := InsertPoet(db)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
 
+func InsertPoet(db *gorm.DB) error {
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	if err := tx.Create(&Poet{Dynasty: "tang", Author: "李白", Paragraph: "意识"}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
 }
